281f2c21246e5bd1344df034364fdafa36c296bd
springhack <springhack@live.cn>
2016-06-01 12:49:29 +0800

    Fix installation

--- classes/DataPoster.php
+++ classes/DataPoster.php
@@ -13,27 +13,6 @@
 		{
 			//MySQL
 			$this->db = new MySQL();
-			if ($this->db->query("SHOW TABLES LIKE 'Record'")->num_rows() != 1)
-			{
-				$this->db->struct(array(
-						'id' => 'text',
-						'oid' => 'text',
-						'rid' => 'text',
-						'tid' => 'text',
-						'user' => 'text',
-						'time' => 'text',
-						'memory' => 'text',
-						'long' => 'text',
-						'lang' => 'text',
-						'result' => 'text',
-						'oj' => 'text',
-						'oj_u' => 'text',
-						'oj_p' => 'text',
-						'code' => 'longtext',
-						'contest' => 'text'
-					))->create("Record");
-			}
-			
 			require_once(dirname(__FILE__)."/".$oj."_DataPoster.php");
 			$str = $oj."_DataPoster";
 			$this->poster = new $str("skvj01", "forskvj", $id, $lang, $code, $cid);
