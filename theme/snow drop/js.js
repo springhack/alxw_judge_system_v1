4161dbc40793587e7ed775700aacab5592280da3
springhack <springhack@live.cnb>
2016-06-26 11:29:40 +0800

    Orz...THEME

--- theme/snow drop/js.js
+++ theme/snow drop/js.js
@@ -1,7 +1,7 @@
 /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2016-05-31 09:32:09
-        Filename: theme/snow drop/js.js
+        Last modified: 2016-05-31 09:30:43
+        Filename: js.js
         Description: Created by SpringHack using vim automatically.
 **/
 $(function () {
@@ -10,31 +10,130 @@ $(function () {
 				if ($(this).attr("data-type") != "rank")
 					$(this).addClass("menu");
 			});
-		$(document.body).append("<center><br /><h5><a href='http://www.90its.cn/' target='_blank' style='color: #000000;'>Designed by SpringHack in Cello Studio</a></h5></center>");
+		//$(document.body).append("<center><br /><h5><a href='http://www.90its.cn/' target='_blank' style='color: #000000;'>Designed by SpringHack in Cello Studio</a></h5></center>");
 		$('.navigator.contest').css('padding-bottom', '20px').parent().css('padding-bottom', '40px')
+		$(document.body).attr('id', 'particles-js');
+		$.getScript(//'theme/ACG/particles.min.js', 
+		'http://cdn.maxjia.com/js/particles.min.js', function (script, status) {
+			if (status == 'success')
+			particlesJS('particles-js', {
+			    "particles": {
+			      "number": {
+			        "value": 110,
+			        "density": {
+			          "enable": true,
+			          "value_area": 800
+			        }
+			      },
+			      "color": {
+			        "value": "#ffffff"
+			      },
+			      "shape": {
+			        "type": "circle",
+			        "stroke": {
+			          "width": 0,
+			          "color": "#000000"
+			        },
+			        "polygon": {
+			          "nb_sides": 5
+			        },
+			        "image": {
+			          "src": "img/github.svg",
+			          "width": 100,
+			          "height": 100
+			        }
+			      },
+			      "opacity": {
+			        "value": 0.5,
+			        "random": false,
+			        "anim": {
+			          "enable": false,
+			          "speed": 1,
+			          "opacity_min": 0.1,
+			          "sync": false
+			        }
+			      },
+			      "size": {
+			        "value": 1,
+			        "random": true,
+			        "anim": {
+			          "enable": false,
+			          "speed": 20,
+			          "size_min": 0.1,
+			          "sync": false
+			        }
+			      },
+			      "line_linked": {
+			        "enable": true,
+			        "distance": 40,
+			        "color": "#fff",
+			        "opacity": 1,
+			        "width": 1
+			      },
+			      "move": {
+			        "enable": true,
+			        "speed": 3,
+			        "direction": "none",
+			        "random": false,
+			        "straight": false,
+			        "out_mode": "out",
+			        "attract": {
+			          "enable": false,
+			          "rotateX": 600,
+			          "rotateY": 1200
+			        }
+			      }
+			    },
+			    "interactivity": {
+			      "detect_on": "canvas",
+			      "events": {
+			        "onhover": {
+			          "enable": true,
+			          "mode": "grab"
+			        },
+			        "onclick": {
+			          "enable": true,
+			          "mode": "push"
+			        },
+			        "resize": true
+			      },
+			      "modes": {
+			        "grab": {
+			          "distance": 120,
+			          "line_linked": {
+			            "opacity": 1
+			          }
+			        },
+			        "bubble": {
+			          "distance": 400,
+			          "size": 40,
+			          "duration": 2,
+			          "opacity": 8,
+			          "speed": 3
+			        },
+			        "repulse": {
+			          "distance": 300
+			        },
+			        "push": {
+			          "particles_nb": 4
+			        },
+			        "remove": {
+			          "particles_nb": 2
+			        }
+			      }
+			    },
+			    "retina_detect": true,
+			    "config_demo": {
+			      "hide_card": false,
+			      "background_color": "#b61924",
+			      "background_image": "",
+			      "background_position": "50% 50%",
+			      "background_repeat": "no-repeat",
+			      "background_size": "cover"
+			    }
+			  }
+			
+			);	
+		});
 	});
 
-$(function(){
-  			var d="<div class='snow'>❅<div>"
-			var timer = setInterval(function () {
-				var f = $(document).width();
-				var e = Math.random()*f - 100;
-				var o = 0.3 + Math.random();
-				var fon = 1 + Math.random()*20;
-				var l  =  e - 100 + 200 * Math.random();
-				var k = 2000 + 5000 * Math.random();
-				$(d).clone().appendTo($(document.body)).css({
-					left: e + "px",
-					opacity: o,
-					"font-size": fon,
-				}).animate({
-				  top: $(document.body).height(),
-					left: l + "px",
-					opacity: 0.1,
-				},k, "linear", function () {$(this).remove()})
-			},200)
-			window.stopSnow = function () {
-				clearInterval(timer);
-				$('.snow').remove();
-			};
-});
